754f72b8073210aeadfc9da08a0f3de1145d1bf0
yudum <yudum@YUDUM>
2026-03-05 20:50:53 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double totalWeighted = 0;
+        int totalCredits = 0;
+
+        Console.Write("Kaç ders gireceksiniz? ");
+        int courseCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < courseCount; i++)
+        {
+            Console.WriteLine("\n" + (i + 1) + ". Ders");
+
+            Console.Write("Kredi: ");
+            int credit = int.Parse(Console.ReadLine());
+
+            Console.Write("Not katsayısı: ");
+            double grade = double.Parse(Console.ReadLine());
+
+            double weighted = credit * grade;
+
+            Console.WriteLine("Hesaplanan: " + weighted);
+
+            totalWeighted += weighted;
+            totalCredits += credit;
+        }
+
+        double gpa = totalWeighted / totalCredits;
+
+        Console.WriteLine("\nToplam Ağırlıklı: " + totalWeighted);
+        Console.WriteLine("Toplam Kredi: " + totalCredits);
+        Console.WriteLine("Dönem Ortalaması: " + gpa);
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("GPACalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GPACalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("011cdb74-9cd4-420d-97f3-3244ebcaf290")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
